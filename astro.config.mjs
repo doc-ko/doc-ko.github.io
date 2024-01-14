1b33288e525f99b1970401a545b561d3dce90b5a
Karl Oberio <git@docko.dev>
2024-01-14 17:02:46 +1300

    Added markdoc support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://oberio.nz',
+  site: 'https://oberio.nz',
+  integrations: [markdoc()]
 });
